8b7a01a838cb9189bf3e5cb2c7898a10ed34a396
KingSwing <tkic37@gmail.com>
2018-08-14 11:42:10 -0700

    Insert Game Files
    I put the game files into the game's folder.

---
+++ Project 2/Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour {
+
+    public float Speed;
+    private Transform PlayerPos;
+
+	// Use this for initialization
+	void Start () {
+        PlayerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, PlayerPos.position, Speed * Time.deltaTime);
+	}
+}
---
+++ Project 2/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public float Speed;
+    private Vector2 MoveVelocity;
+
+    // Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MoveVelocity = moveInput.normalized * Speed;
+    }
+    void FixedUpdate ()
+    {
+        //Accesses Rigidbody2D
+        Rigidbody2D rb2D = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+        //Applies the movement to the player
+        rb2D.velocity = (MoveVelocity * Time.fixedDeltaTime);
+    }
+
+}
